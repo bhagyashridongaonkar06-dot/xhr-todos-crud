55e8f1be59510b2f6d2f18deef76df7d15c75f03
bhagyashridongaonkar06-dot <bhagyashridongaonkar06@gmail.com>
2026-06-12 13:03:07 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,228 @@
+const cl = console.log;
+
+let base_url ='https://jsonplaceholder.typicode.com'
+let todo_url = `${base_url}/todos`
+
+const todoform = document.getElementById('todoform')
+const title = document.getElementById('title')
+const completed = document.getElementById('completed')
+const userId = document.getElementById('userId')
+const todoContainer = document.getElementById('todoContainer')
+const addTodo = document.getElementById('addTodo')
+const updateTodo = document.getElementById('updateTodo')
+
+
+let todoArr = [];
+
+function snackbar(msg,icon){
+    swal.fire({
+        title : msg,
+        icon :icon,
+        timer : 4000
+    })
+}
+
+function fetchData(){
+    let todo_url = `${base_url}/todos`
+ 
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', todo_url)
+    xhr.send(null)
+
+    xhr.onload = function(){
+        cl(xhr.response);
+        cl(xhr.status);
+
+        if(xhr.status >= 200 && xhr.status <=299){
+            todoArr = JSON.parse(xhr.response)
+            // cl(data);
+            createCard(todoArr)
+        }
+    }
+}
+
+fetchData()
+
+function createCard(arr){
+    let res = '';
+
+    arr.forEach(ele => {
+        res += `<div class="col-md-6 mb-4" id=${ele.id}>
+                <div class="card">
+                    <div class="card-body">
+                        <h4>
+                            title : <span class="title">${ele.title}</span>
+                        </h4>
+                        <h5>
+                            <strong>status:</strong>
+                          
+                            <span class="badge ${ele.completed ?"badge-success" :"badge-danger"}">
+                                   ${ele.completed ?"completed" : "pending"}
+                            </span> 
+                        </h5>
+                        <div class="card-footer d-flex justify-content-between">
+                            <button class="btn btn-sm btn-outline-primary" onclick="onedit(this)">Edit</button>
+                            <button class="btn btn-sm btn-outline-danger" onclick="ondelete(this)">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        
+    });
+    todoContainer.innerHTML = res;
+}
+
+function onSubmit(ele){
+    ele.preventDefault();
+
+    let newObj={
+        title : title.value,
+        completed : completed.value,
+        userId : userId.value
+    }
+    todoArr.push(newObj)
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', todo_url)
+    xhr.send(JSON.stringify(newObj));
+
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status <= 299){
+            let res = JSON.parse(xhr.response)
+            let col = document.createElement('div');
+            col.className = 'col-md-6';
+            col.id = res.id;
+            col.innerHTML = `
+                    <div class="card-body">
+                        <h4>
+                            title : <span class="title">${newObj.title}</span>
+                        </h4>
+                        <h5>
+                            <strong>status:</strong>
+                          
+                            <span class="badge ${newObj.completed ?"badge-success" :"badge-danger"}">
+                                   ${newObj.completed ?"completed" : "pending"}
+                            </span> 
+                        </h5>
+                        <div class="card-footer d-flex justify-content-between">
+                            <button class="btn btn-sm btn-outline-primary" onclick="onedit(this)">Edit</button>
+                            <button class="btn btn-sm btn-outline-danger" onclick="ondelete(this)">Delete</button>
+                        </div>
+                    </div>
+                </div>`
+                todoContainer.prepend(col);
+        }else{
+        snackbar('failed to submit data', 'error')
+        }
+    }
+}
+
+function onedit(ele){
+    let editId = ele.closest('.col-md-6').id;
+    localStorage.setItem('editId', editId)
+
+    let edit_url =` ${base_url}/todos/${editId}`
+    document.querySelectorAll('.btn-outline-danger').forEach(element => {
+        element.disabled = true;
+    });
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', edit_url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('Autho', 'get token from');
+
+    xhr.send(null);
+
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status <= 299){
+             let editObj = JSON.parse(xhr.response);
+           cl(editObj);
+            title.value = editObj.title;
+            completed.value = editObj.completed;
+            userId.value = editObj.userId;
+            addTodo.classList.add('d-none');
+            updateTodo.classList.remove('d-none');
+        }else{
+            snackbar('something went wrong', 'error')
+        }
+
+    }
+}
+
+function onupdateTodo(eve){
+    let updateId = localStorage.getItem('editId')
+    let update_url = `${base_url}/todos/${updateId}`
+
+    let updatedObj={
+        title : title.value,
+        completed : completed.value,
+        userId : userId.value,
+        id : updateId
+    }
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('PATCH', update_url)
+    xhr.send(JSON.stringify(updatedObj));
+
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status <= 299){
+            let div = document.getElementById(updateId);
+            div.innerHTML = `<div class="card-body">
+                        <h4>
+                            title : <span class="title">${updatedObj.title}</span>
+                        </h4>
+                        <h5>
+                            <strong>status:</strong>
+                          
+                            <span class="badge ${updatedObj.completed ?"badge-success" :"badge-danger"}">
+                                   ${updatedObj.completed ?"completed" : "pending"}
+                            </span> 
+                        </h5>
+                        <div class="card-footer d-flex justify-content-between">
+                            <button class="btn btn-sm btn-outline-primary" onclick="onedit(this)">Edit</button>
+                            <button class="btn btn-sm btn-outline-danger" onclick="ondelete(this)">Delete</button>
+                        </div>
+                    </div>
+                </div>`
+
+                addTodo.classList.remove('d-none');
+                updateTodo.classList.add('d-none');
+                todoform.reset();
+                document.querySelectorAll('.btn-outline-danger').forEach(element => {
+                element.disabled = false;
+    });
+
+        }else{
+            snackbar('failed to edit data', 'error')
+        }
+
+
+    }
+}
+
+function ondelete(ele){
+    let removeId = ele.closest('.col-md-6').id;
+
+    let remove_url = `${base_url}/todos/${removeId}`;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('DELETE', remove_url);
+    xhr.send(null);
+
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status <= 299){
+            ele.closest('.col-md-6').remove()
+        }else{
+            snackbar('failed to delete data', 'error')
+        }
+    }
+}
+
+todoform.addEventListener('submit', onSubmit);
+updateTodo.addEventListener('click', onupdateTodo)
+
